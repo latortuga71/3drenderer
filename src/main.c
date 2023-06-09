e5bef6343876869e8bdd9a2aa495251817496541
christopher alonso <calonso@christophers-MacBook-Pro.local>
2023-06-09 11:48:50 -0400

    rotating all axis

--- src/main.c
+++ src/main.c
@@ -8,6 +8,12 @@
 // declare vectors/points for cube
 const int N_POINTS = 9*9*9;
 vec3_t cube_points[N_POINTS]; // 9x9x9 cube
+vec2_t projected_points[N_POINTS];                              
+
+vec3_t camera_position = { .x = 0, .y = 0, .z = -5};
+vec3_t cube_rotation = { .x = 0, .y = 0, .z = 0 };
+//float fov_factor = 128;
+float fov_factor = 640;
 
 void setup(void) {
     // window width and height
@@ -53,17 +59,48 @@ void process_input(void) {
     }
 }
 
+// 3d vector return projected 2d point
+vec2_t project(vec3_t point){
+    vec2_t projected_point = {
+        .x = (fov_factor * point.x) / point.z,
+        .y = (fov_factor * point.y) / point.z,
+    };
+    return projected_point;
+}
+
+
 void update(void) {
-    /// to do
+    cube_rotation.y += 0.01;
+    cube_rotation.x += 0.01;
+    cube_rotation.z += 0.01;
+    for (int i = 0; i < N_POINTS; i++){
+        vec3_t point = cube_points[i];
+        // rotate point
+        vec3_t transformed_point = vec3_rotate_y(point,cube_rotation.y);
+        transformed_point = vec3_rotate_x(transformed_point,cube_rotation.x);
+        transformed_point = vec3_rotate_z(transformed_point,cube_rotation.z);
+        // translate move point away from camera
+        transformed_point.z -= camera_position.z;
+        // project current point
+        vec2_t projected_point = project(transformed_point);
+        // save point
+        projected_points[i] = projected_point;
+    }
 }
 
 
 void render(void) {
-    SDL_SetRenderDrawColor(renderer,255,0,0,255);
-    SDL_RenderClear(renderer);
-    draw_grid();
-    draw_pixel(20,20,0xFFFFFF00);
-    draw_rect(300,200,300,150,0xFFFF00FF);
+    //SDL_SetRenderDrawColor(renderer,255,0,0,255);
+    //SDL_RenderClear(renderer);
+    //draw_grid();
+    // loop all projected points and render them
+    //
+    for (int i = 0; i < N_POINTS; i++){
+        vec2_t projected_point = projected_points[i];
+        draw_rect(projected_point.x + (window_width / 2) ,projected_point.y + (window_height / 2),4,4,0xFFFFFF00);
+    }
+    //draw_pixel(20,20,0xFFFFFF00);
+    //draw_rect(300,200,300,150,0xFFFF00FF);
     render_color_buffer();
     clear_color_buffer(0xFF000000);
     SDL_RenderPresent(renderer);
