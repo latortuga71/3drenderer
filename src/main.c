fd97b87f62d7deb47dcd668255470f82ae25ddfd
christopher alonso <calonso@dhcp-10-20-215-103.nvidia.com>
2023-06-20 15:45:37 -0400

    bubble sort painter algorithim to sort triangle faces by depth

--- src/main.c
+++ src/main.c
@@ -151,17 +151,31 @@ void update(void) {
             projected_points[j].y += (window_height / 2);
             //projected_triangle.points[j] = projected_point;
         }
+        // calculate average depth for each face based on vertices after transformation z values
+        float avg_depth = (transformed_vertices[0].z + transformed_vertices[1].z + transformed_vertices[2].z) / 3.0;
         triangle_t projected_triangle = {
             .points = {
                 { projected_points[0].x, projected_points[0].y},
                 { projected_points[1].x, projected_points[1].y},
                 { projected_points[2].x, projected_points[2].y},
             },
-            .color = mesh_face.color
+            .color = mesh_face.color,
+            .avg_depth = avg_depth
         };
         // save the projected triangle in the array of triangles to render.
         //triangles_to_render[i] = projected_triangle;
         array_push(triangles_to_render,projected_triangle);
+    }
+    // sort triangles to render by their average depth ascending.
+    int num_triangles = array_length(triangles_to_render);
+    for (int i = 0; i < num_triangles; i++){
+        for (int j = 0; j < num_triangles; j++){
+            if (triangles_to_render[i].avg_depth < triangles_to_render[j].avg_depth){
+                triangle_t temp = triangles_to_render[i];
+                triangles_to_render[i] = triangles_to_render[j];
+                triangles_to_render[j] = temp;
+            }
+        }
     }
 }
 
@@ -169,6 +183,7 @@ void update(void) {
 void render(void) {
     draw_grid();
     // how many triangles in dynamic array
+    // triangles should be sorted by depth
     int num_triangles = array_length(triangles_to_render);
     for (int i = 0; i < num_triangles; i++){
         triangle_t triangle = triangles_to_render[i];
